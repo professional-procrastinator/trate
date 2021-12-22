287c2d32f79137f7b91d11c4adb9e789a2095cfa
Haunter-123 <jhanishitback@gmail.com>
2021-12-22 18:27:37 +0530

    ok might work i think

--- client/pages/login.js
+++ client/pages/login.js
@@ -20,7 +20,7 @@ export default function Login({ providers }) {
                 <p className={styles.description}>Get started today.</p>
                
                         <div>
-                            <div onClick={() => signIn()} className={styles.loginButton}>
+                            <div onClick={() => signIn('google')} className={styles.loginButton}>
                                 <Image src={GLogo} className={styles.GLogo} />
                                 <p>Login with Google</p>
                             </div>
