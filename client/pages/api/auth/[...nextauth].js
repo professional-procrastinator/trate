cc6524217b40ec2400c9e464c65a1acd606bea1a
Haunter-123 <jhanishitback@gmail.com>
2021-12-23 11:48:46 +0530

    frick vercel 2

--- client/pages/api/auth/[...nextauth].js
+++ client/pages/api/auth/[...nextauth].js
@@ -24,8 +24,6 @@ export default NextAuth({
         
       },
       async signIn(req, userAcc) {
-        console.log(req,userAcc)
-        
         const id_token = userAcc.id_token; //get id token from the request headers
         const response = await fetch(`https://oauth2.googleapis.com/tokeninfo?id_token=${id_token}`) //verify token, and get user details from google oauth
 
